ae3f108c5e5fccde1bb0ba9388a1b1fa83d94a7a
AhmadOthman1 <103585755+AhmadOthman1@users.noreply.github.com>
2022-05-05 20:18:30 +0300

    Ahmad

--- RMenu.php
+++ RMenu.php
@@ -48,9 +48,12 @@ catch (Exception $ex){
 
 
 <?php
-$disableEditItem="none";
-$disableErrorPrice="none";
+$disableEditItemDiv="";
 $errormsg="";
+$disableErrorPriceDiv="";
+
+
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST['deleteMenuItem'])) {
         $conn = new mysqli('localhost', 'root', '', 'food4u');
@@ -60,8 +63,103 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
         $conn->close();
     }
+
+    if (isset($_POST['CancelMenuItem'])) {
+        $disableEditItemDiv="";
+    }
+    if (isset($_POST['errorOkButton'])) {
+        $disableErrorPriceDiv="";
+        $errormsg="";
+    }
+    if (isset($_POST['SaveMenuItem'])) {
+        $nItem=$_POST['MenuItemName'];
+        $dItem=$_POST['MenuItemDes'];
+        $pItem=$_POST['MenuItemPrice'];
+        $iItem=$_POST['MenuItemId'];
+
+        if(!empty($nItem) && !empty($dItem) && !empty($pItem)){
+            $disableErrorPriceDiv="";
+            $errormsg="";
+            if(is_numeric($_POST['MenuItemPrice'])){
+                $disableErrorPriceDiv="";
+                $errormsg="";
+                $conn = new mysqli('localhost', 'root', '', 'food4u');
+                $qrstr="UPDATE `meals` SET `name`='".$nItem."',`description`='".$dItem."',`price`='".abs($pItem)."' WHERE `id`='".$iItem."'";
+                $conn->query($qrstr);
+                $conn->close();
+                if(isset($_FILES['MenuItemImage']['name'])) {
+                    $fileName = $_FILES['MenuItemImage']['name'];
+                    $fileType = pathinfo($fileName, PATHINFO_EXTENSION);
+                    $allowTypes = array('jpg','png','jpeg');
+                    if(in_array($fileType, $allowTypes) && $_FILES['MenuItemImage']['size'] < 200000){
+                        $conn = new mysqli('localhost', 'root', '', 'food4u');
+                        $image = $_FILES['MenuItemImage']['tmp_name'];
+                        $imgContent = addslashes(file_get_contents($image));
+                        $qrstr = "UPDATE `meals` SET `image`='".$imgContent."' WHERE `id`='".$iItem."'";
+                        $conn->query($qrstr);
+                    }else{
+                    }
+                }
+                else{
+                    $disableEditItemDiv="";
+                }
+                $disableEditItemDiv="";
+            }
+            else{// print\show html error div
+                $errormsg="Please Enater Valid Price";
+                $disableErrorPriceDiv="
+ <div class='errorMenuItem ' >
+    <div class='errorMenuItem2 container h-100' >
+        <div class='row align-items-center h-100' >
+            <div class='col-md-2' ></div>
+
+            <div class='col-md-8 mx-auto'>
+                <div class='errorMenuItemContent' style='align: center'>
+                    <form method='POST' action=".$_SERVER["PHP_SELF"].">
+                        <table style='width: 100%; border-collapse: separate; border-spacing: 0 20px;'>
+                            <tr><td style='text-align: center; vertical-align: middle;'><input class='errorTextField ' disabled type='text' name='ErrorPrice' value='$errormsg'></td></tr>
+                            <tr><td style='text-align: center; vertical-align: middle;'><input type='submit' class='blackSquaredButton' name='errorOkButton' style='width:50%; height:50px' value='Ok'></td></tr>
+                        </table>
+                    </form>
+                </div>
+            </div>
+            <div class='col-md-2' ></div>
+        </div>
+    </div>
+</div> 
+" ;
+
+            }
+        }
+        else{
+            $errormsg="Please Enater Valid Name / description";
+            $disableErrorPriceDiv="
+ <div class='errorMenuItem ' >
+    <div class='errorMenuItem2 container h-100' >
+        <div class='row align-items-center h-100' >
+            <div class='col-md-2' ></div>
+
+            <div class='col-md-8 mx-auto'>
+                <div class='errorMenuItemContent' style='align: center'>
+                    <form method='POST' action=".$_SERVER["PHP_SELF"].">
+                        <table style='width: 100%; border-collapse: separate; border-spacing: 0 20px;'>
+                            <tr><td style='text-align: center; vertical-align: middle;'><input class='errorTextField ' disabled type='text' name='ErrorPrice' value='$errormsg'></td></tr>
+                            <tr><td style='text-align: center; vertical-align: middle;'><input type='submit' class='blackSquaredButton' name='errorOkButton' style='width:50%; height:50px' value='Ok'></td></tr>
+                        </table>
+                    </form>
+                </div>
+            </div>
+            <div class='col-md-2' ></div>
+        </div>
+    </div>
+</div> 
+" ;
+        }
+
+    }
+}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST['editMenuItem'])) {
-        $disableEditItem="bolck";
         $conn = new mysqli('localhost', 'root', '', 'food4u');
         $qrstr2="SELECT `id`, `name`, `description`, `price`, `section`, `image` FROM `meals` WHERE `id`='". $_POST['MenuItemId']."'";
         $res2=$conn->query($qrstr2);
@@ -71,41 +169,66 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mdescription = $row2->description;
         $mprice = $row2->price;
         $mid = $row2->id;
+        $disableEditItemDiv="<div class='editMenuItem '>
+    <div class='editMenuItem2 container h-100''>
+        <div class='row align-items-center h-100' >
+            <div class='col-md-2' ></div>
+
+                <div class='col-md-8 mx-auto'>
+                    <div class='editMenuItemContent'>
+
+                        <h5 style='text-align: center'>Edit Menu Item</h5>
+                        <form method='POST' action='". $_SERVER['PHP_SELF']."' enctype='multipart/form-data'>
+
+                            <table style='width: 80%;'>
+                                <tr style='display: none'>
+                                    <td><input class='activTextField' type='text' name='MenuItemId' value='$mid'></td>
+                                </tr>
+                                <tr>
+                                    <td><label for='MenuItemName'>Name:</label></td>
+                                    <td><input class='activTextField' type='text' name='MenuItemName' value='$mName'></td>
+                                </tr>
+                                <tr>
+                                    <td><label for='MenuItemDes'>Description: </label></td>
+                                    <td><textarea cols='20' rows='4' style='resize: none;' class='activTextField' name='MenuItemDes' > $mdescription</textarea></td>
+                                </tr>
+                                <tr>
+                                    <td><label for='MenuItemPrice'></label>Price: </td>
+                                    <td><input class='activTextField' type='text' name='MenuItemPrice' value='$mprice'></td>
+                                </tr>
+                                <tr>
+                                    <td><label >Item Image:</label></td>
+                                    <td><input type='file' name='MenuItemImage' id='MenuItemImage' accept='.jpg,.jpeg,.png'><h6 style='color: gray'>image must be less than 200KB</h6></td>
+
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td>
+                                        <table style='width: 100%'>
+                                            <tr>
+                                                <td>
+                                                    <input name='SaveMenuItem' class='blackSquaredButton' style='width: 90%;height: 50px ;margin-top: 10px' type='submit' value='Save' >
+                                                </td>
+                                                <td>
+                                                    <input name='CancelMenuItem' class='blackSquaredButton' style='width: 90%;height: 50px ;margin-top: 10px' type='submit' value='Cancel' >
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </table>
+
+
+                        </form>
+
+                    </div>
+                </div>
+
+            <div class='col-md-2'></div>
+        </div>
+    </div></div >";
+
         $conn->close();
-    }
-    if (isset($_POST['CancelMenuItem'])) {
-        $disableEditItem="none";
-    }
-    if (isset($_POST['errorOkButton'])) {
-        $disableErrorPrice="none";
-        $errormsg="";
-    }
-    if (isset($_POST['SaveMenuItem'])) {
-        $nItem=$_POST['MenuItemName'];
-        $nDes=$_POST['MenuItemDes'];
-        $nprice=$_POST['MenuItemPrice'];
-        if(!empty($nItem) && !empty($nDes) && !empty($nprice)){
-            $disableErrorPrice="none";
-            $errormsg="";
-            if(is_numeric($_POST['MenuItemPrice'])){
-                $disableErrorPrice="none";
-                $errormsg="";
-                $conn = new mysqli('localhost', 'root', '', 'food4u');
-                $qrstr="SELECT `id`, `name`, `description`, `price`, `section`, `image` FROM `meals` WHERE `id`='".$mid."'";
-
-                $disableEditItem="none";
-            }
-            else{
-                $disableErrorPrice="block";
-                $errormsg="Please Enater Valid Price";
-
-            }
-        }
-        else{
-            $disableErrorPrice="block";
-            $errormsg="Please Enater Valid Name / description";
-        }
-
     }
 }
 ?>
@@ -136,80 +259,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </script>
 </head>
 <!-- MENU -->
-<div class="editMenuItem " style="display: <?php echo $disableEditItem;?>;">
-    <div class="editMenuItem2 container h-100"">
-        <div class="row align-items-center h-100" >
-            <div class="col-md-2" ></div>
-
-                <div class="col-md-8 mx-auto">
-                    <div class="editMenuItemContent">
-
-                        <h5 style="text-align: center">Edit Menu Item</h5>
-                        <form method="POST" action="<?php echo $_SERVER["PHP_SELF"]?>">
-
-                            <table style="width: 80%;">
-                                <tr>
-                                    <td><label for="MenuItemName">Name:</label></td>
-                                    <td><input class="activTextField" type="text" name="MenuItemName" value="<?php echo $mName ?>"></td>
-                                </tr>
-                                <tr>
-                                    <td><label for="MenuItemDes">Description: </label></td>
-                                    <td><textarea cols="20" rows="4" class="activTextField" name="MenuItemDes" ><?php echo $mdescription ?></textarea></td>
-                                </tr>
-                                <tr>
-                                    <td><label for="MenuItemPrice"></label>Price: </td>
-                                    <td><input class="activTextField" type="text" name="MenuItemPrice" value="<?php echo $mprice ?>"></td>
-                                </tr>
-                                <tr>
-                                    <td><label >Item Image:</label></td>
-                                    <td><input type="file" accept=".jpg,.jpeg,.png"></td>
-                                </tr>
-                                <tr>
-                                    <td></td>
-                                    <td>
-                                        <table style="width: 100%">
-                                            <tr>
-                                                <td>
-                                                    <input name="SaveMenuItem" class="blackSquaredButton" style="width: 90%;height: 50px ;margin-top: 10px" type="submit" value="Save" >
-                                                </td>
-                                                <td>
-                                                    <input name="CancelMenuItem" class="blackSquaredButton" style="width: 90%;height: 50px ;margin-top: 10px" type="submit" value="Cancel" >
-                                                </td>
-                                            </tr>
-                                        </table>
-                                    </td>
-                                </tr>
-                            </table>
-
-
-                        </form>
-
-                    </div>
-                </div>
-
-            <div class="col-md-2"></div>
-        </div>
-    </div>
-</div >
-<div class="errorMenuItem " style="display: <?php echo $disableErrorPrice;?>;">
-    <div class="errorMenuItem2 container h-100" >
-        <div class="row align-items-center h-100" >
-            <div class="col-md-2" ></div>
-
-            <div class="col-md-8 mx-auto">
-                <div class="errorMenuItemContent" style="align: center">
-                    <form method="POST" action="<?php echo $_SERVER["PHP_SELF"]?>">
-                        <table style="width: 100%; border-collapse: separate; border-spacing: 0 20px;">
-                            <tr><td style="text-align: center; vertical-align: middle;"><input class="errorTextField " disabled type="text" name="ErrorPrice" value="<?php echo $errormsg ?>"></td></tr>
-                            <tr><td style="text-align: center; vertical-align: middle;"><input type="submit" class="blackSquaredButton" name="errorOkButton" style="width:50%; height:50px" value="Ok"></td></tr>
-                        </table>
-                    </form>
-                </div>
-            </div>
-            <div class="col-md-2" ></div>
-        </div>
-    </div>
-</div>
+<?php echo $disableEditItemDiv; ?>
+<?php echo $disableErrorPriceDiv; ?>
 <section class="nd-flex justify-content-end custom-navbar navbar-fixed-top navbarStyle fixed-top " role="navigation">
     <div  class="navbar navbar-expand-lg main-nav px-0 ">
         <div class="container-fluid">
@@ -269,7 +320,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                     for($i=0;$i<$res->num_rows;$i++) {
                                         $row = $res->fetch_object();
                                         $sName = $row->sName;
-                                        echo '<tr><td><h1 class="sectionName" style="text-align: center">'.$sName.'</h1></td></tr>';
+                                        echo '<tr><td><h1 class="sectionName" style="text-align: center">'.$sName.'</h1>
+                                        <form method="POST" action="'.$_SERVER["PHP_SELF"].'">
+                                            <input type="text" name="secId" value="'.$sName.'" style="display: none;">
+                                            <input type="submit" name="chanegSecName" class="greenUnborderedButton" value="Change">
+                                            <input type="submit" name="deleteSecName" class="greenUnborderedButton" value="Delete">
+                                        </form>
+                                        </td>
+                                        ';
                                         $qrstr2="SELECT `id`, `name`, `description`, `price`, `section`, `image` FROM `meals` WHERE `Email`='".$_SESSION['Email']."' and `section`='".$sName."'";
                                         $res2=$conn->query($qrstr2);
                                         for($j=0;$j<$res2->num_rows;$j++) {
@@ -286,7 +344,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                                 <img class="mealImage" src="data:image/jpeg;base64,'.base64_encode($mimage).'"/>
                                                 <div class="mealInfo">
                                                     <h3 class="mealName">'.$mName.'</h3>
-                                                    <p class="mealDes">'.$mdescription.'</p>
+                                                    <p class="mealDes" style="max-width:600px;word-break: break-all; white-space: normal;">'.$mdescription.'</p>
                                                     <form method="POST" action="'.$_SERVER["PHP_SELF"].'">
                                                         <input type="text" name="MenuItemId" value="'.$mid.'" style="display: none">
                                                         <input name="deleteMenuItem" class="greenUnborderedButton" type="submit" value="Delete" >
